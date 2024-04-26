36ec5f7becfc5caf123ccf1216c5d2c22f3b25c0
ingarbi <104501854+ingarbi@users.noreply.github.com>
2024-04-26 20:29:31 +0300

    rendering

--- app/properties/(crud)/[id]/page.jsx
+++ app/properties/(crud)/[id]/page.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+"use client"
+
+import { useParams, useSearchParams, usePathname } from "next/navigation";
 
 const PropertyPage = () => {
+  const params = useParams();
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const name = searchParams.get("name");
   return (
-    <div>PropertyPage</div>
+    <div>PropertyPage, {params.id}, ---{name}, {pathName}</div>
   )
 }
 
